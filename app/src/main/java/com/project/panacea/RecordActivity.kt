90c3cf1b1c2e8f6b6b036dd4a8b0eac5f310033b
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2024-05-17 01:59:54 +0600

    additional activities created

--- app/src/main/java/com/project/panacea/RecordActivity.kt
+++ app/src/main/java/com/project/panacea/RecordActivity.kt
@@ -1,9 +1,13 @@
 package com.project.panacea
 
+import android.app.Dialog
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -13,13 +17,27 @@ class RecordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_record)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-        var addBtn = findViewById<Button>(R.id.add_new);
-        addBtn.setOnClickListener {
+
+        val systolicPressure = HeartRateData.getSystolicPressure()
+        val diastolicPressure = HeartRateData.getDiastolicPressure()
+
+        var addNewBtn = findViewById<Button>(R.id.add_new);
+        var statBtn = findViewById<Button>(R.id.stats);
+        addNewBtn.setOnClickListener {
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.addnew_dialog)
+            val spinner: Spinner = dialog.findViewById(R.id.spinner)
+            val items = arrayOf("AM", "PM")
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+            spinner.adapter = adapter
+            dialog.findViewById<Button>(R.id.Cancel).setOnClickListener {
+                dialog.dismiss()
+            }
+
+            dialog.findViewById<Button>(R.id.Add).setOnClickListener {
+
+            }
+
 
         }
     }
